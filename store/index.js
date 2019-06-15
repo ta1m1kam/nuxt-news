f82519b7ba1b36f60a6b7205766b283c518fdef3
TaigaMikami <mikamimikami60@gmail.com>
2019-06-15 14:50:45 +0900

    change country

--- store/index.js
+++ store/index.js
@@ -3,21 +3,38 @@ import Vuex from 'vuex'
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      headlines: []
+      headlines: [],
+      category: '',
+      country: 'us',
+      loading: false
     },
     mutations: {
       setHeadlines(state, headlines) {
         state.headlines = headlines
+      },
+      setLoading(state, loading) {
+        state.loading = loading
+      },
+      setCategory(state, category) {
+        state.category = category
+      },
+      setCountry(state, country) {
+        state.country = country
       }
     },
     actions: {
       async loadHeadlines({ commit }, apiUrl) {
+        commit('setLoading', true)
         const { articles } = await this.$axios.$get(apiUrl)
+        commit('setLoading', false)
         this.commit('setHeadlines', articles)
       }
     },
     getters: {
-      headlines: state => state.headlines
+      headlines: state => state.headlines,
+      category: state => state.category,
+      loading: state => state.loading,
+      country: state => state.country
     }
   })
 }
